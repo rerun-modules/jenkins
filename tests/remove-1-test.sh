f2fb1b9e9a8663642cf6cffc46049d5aa1085f05
connaryscott <connaryscott@gmail.com>
2012-11-13 14:35:51 -0800

    add non default test for cleanup

--- tests/remove-1-test.sh
+++ tests/remove-1-test.sh
@@ -13,4 +13,19 @@ describe "remove"
 
 it_runs_without_arguments() {
     rerun jenkins:remove
+    rerun jenkins:install
+    rerun jenkins:remove
+
+    test -f /etc/yum.repos.d/jenkins.repo.rpmsave  && { true; } || { false; }
+}
+
+it_runs_with_cleanup() {
+    rerun jenkins:remove
+    rerun jenkins:install
+    rerun jenkins:remove --cleanup true
+    
+    test -e /var/cache/jenkins && { false; } || { true; }
+    test -e /var/log/jenkins  && { false; } || { true; }
+    test -e /etc/yum.repos.d/jenkins.repo.rpmsave  && { false; } || { true; }
+
 }
