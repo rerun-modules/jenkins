bd1b03244960ac641b0a9081a38ed54b1d40fb4a
Anthony Shortland <anthony@dtosolutions.com>
2012-11-14 08:59:05 -0800

    Implements jenkins:build-now
    Closes #20 by providing an initial implementation of the build-now command that posts to the specified job's build API endpoint (e.g. "http://centos62-dukesbank-rerun:8080/job/dtolabs-toolchain-example5-booking-mysql-data-rpm/build").

---
+++ tests/build-now-1-test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env roundup
+#
+# This file contains the test plan for the build-now command.
+# Execute the plan by invoking: 
+#    
+#     rerun stubbs:test -m jenkins -p build-now
+#
+
+# The Plan
+# --------
+
+describe "build-now"
+
+it_fails_without_arguments() {
+    if ! rerun jenkins:build-now
+    then
+      exit 0
+    fi
+}
+
+it_can_build_now() {
+    rerun jenkins:start
+    rerun jenkins:delete-job --jobname rerun-hello-world --force true
+    rerun jenkins:create-job --jobname rerun-hello-world --file $RERUN_MODULES/jenkins/examples/jobs/rerun-hello-world/config.xml
+    rerun jenkins:build-now --jobname rerun-hello-world
+    rerun jenkins:delete-job --jobname rerun-hello-world 
+    rerun jenkins:stop
+}
